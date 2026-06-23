89612d7795be1e8b068ea6c739eb20da9c092229
kde-work <kde-work@mail.ru>
2026-06-23 22:07:41 +0300

    Remove deprecated installation scripts for backend and frontend; update Nginx configuration to use quotes in location regex.

--- install-backend.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker run --rm -v "$(pwd)/backend":/app -w /app composer:2 composer update laravel/sanctum --no-interaction --ignore-platform-reqs
--- scaffold-frontend.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker run --rm -v "$(pwd)":/app -w /app node:24-alpine sh -c "npm install -g @angular/cli@22 && ng new frontend --routing --style=scss --ssr=false --skip-git --defaults"
