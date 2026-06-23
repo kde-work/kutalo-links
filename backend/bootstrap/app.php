b3ffb562231408a517fb9964d5d89bcc3a0d286a
kde-work <kde-work@mail.ru>
2026-06-23 23:05:35 +0300

    Remove unused middleware configuration from application bootstrap file.

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
-use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,9 +10,6 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware) {
-        $middleware->statefulApi();
-    })
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
